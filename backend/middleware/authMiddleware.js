c613f55bd5c659bbe7646224fc7abff9d1d237a2
Deepanjana Mishra <deep2005mis@gmail.com>
2026-03-10 17:50:32 +0530

    Added backend -authentication mkiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "Access denied" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+
+    const decoded = jwt.verify(token, "SECRET_KEY");
+
+    req.user = decoded;
+
+    next();
+
+  } catch (error) {
+
+    res.status(403).json({ message: "Invalid token" });
+
+  }
+
+};
