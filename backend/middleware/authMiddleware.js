c3ca830828fc8a86aad2e0758890bc10a392e57d
Deepanjana Mishra <deep2005mis@gmail.com>
2026-03-19 10:28:21 +0530

    Updated Authentication Middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -12,7 +12,7 @@ export const verifyToken = (req, res, next) => {
 
   try {
 
-    const decoded = jwt.verify(token, "SECRET_KEY");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded;
 
