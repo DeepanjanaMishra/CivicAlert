bcd712f382290596107266082d0a433e3cc783ff
Deepanjana Mishra <deep2005mis@gmail.com>
2026-03-19 10:23:23 +0530

    Updated Database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
 
-    await mongoose.connect("mongodb://127.0.0.1:27017/civicalert");
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
 
