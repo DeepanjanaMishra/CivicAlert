f54b0ff7916c9d36b74e68db3848e88c2697095f
Deepanjana Mishra <deep2005mis@gmail.com>
2026-04-09 02:18:24 +0530

    updated user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { getUsers } from "../controllers/userController.js";
+import { getUsers, updateUserStatus } from "../controllers/userController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
+import { allowRoles } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
-router.get("/users", getUsers);
+// GET ALL USERS (Admin only)
+router.get(
+  "/users",
+  verifyToken,
+  allowRoles("admin"),
+  getUsers
+);
+
+// UPDATE USER STATUS / ROLE
+router.put(
+  "/users/:id",
+  verifyToken,
+  allowRoles("admin"),
+  updateUserStatus
+);
 
 export default router;
